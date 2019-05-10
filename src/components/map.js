7698b2c910c2c4c65407285007b9bc90fe6c1d97
Cristian Restrepo <crestrepo21@gmail.com>
2019-05-10 08:25:46 -0700

    new gitignore

--- src/components/map.js
+++ src/components/map.js
@@ -4,15 +4,18 @@ class Map extends React.Component {
   constructor(props){
     super(props);
     
-    this.mapSrc = `https://maps.googleapis.com/maps/api/staticmap?center=${this.props.location.latitude}%2c%20${this.props.location.longitude}&zoom=13&size=600x300&maptype=roadmap%20%20&key=${process.env.REACT_APP_GEOCODE_API_KEY}`;
+    this.mapSrc = `https://maps.googleapis.com/maps/api/staticmap?center=${this.props.location.latitude}%2c%20${this.props.location.longitude}&zoom=13&size=600x300&maptype=roadmap&key=${process.env.REACT_APP_GEOCODE_API_KEY}`;
   }
   render(){
     return(
       <div>
-        <img src={this.mapSrc} />
+        <img src={this.mapSrc} alt='map of location'/>
       </div>
     );
   }
 }
 
 export default Map;
+
+// `https://maps.googleapis.com/maps/api/staticmap?center=${location.latitude}%2c%20${location.longitude}&zoom=13&size=600x300&maptype=roadmap
+//   &key=${GEOCODE_API_KEY}`
